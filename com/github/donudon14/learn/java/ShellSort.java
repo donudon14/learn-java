272d9d816878f0cf44fb5b9012f2db2afedcd249
Anatoliy Ruslanov <k1ck001@mail.ru>
2023-04-25 22:31:18 +0100

    rotate

--- com/github/donudon14/learn/java/ShellSort.java
+++ com/github/donudon14/learn/java/ShellSort.java
@@ -1,14 +1,27 @@
 package com.github.donudon14.learn.java;
 
 import java.util.Comparator;
-
+import static com.github.donudon14.learn.java.Arrays.indexOfMin;
+import static com.github.donudon14.learn.java.Arrays.swap;
+import static java.lang.Math.min;
 import static java.util.Arrays.asList;
 import static java.util.Objects.checkFromToIndex;
 
-import static com.github.donudon14.learn.java.Arrays.indexOfMin;
-import static com.github.donudon14.learn.java.Arrays.swap;
-
 public final class ShellSort {
+    private static final int[] GAPS = new int[328];
+
+    static {
+        GAPS[0] = 1;
+        for (int i = 1, i2 = 0, i3 = 0; i < GAPS.length; ++i) {
+            final int n2 = 2 * GAPS[i2], n3 = 3 * GAPS[i3];
+            GAPS[i] = min(n2, n3);
+            if (n2 <= n3)
+                ++i2;
+            if (n2 >= n3)
+                ++i3;
+        }
+    }
+
     public static <T> void sort(
         final T[] array,
         final Comparator<? super T> comparator
