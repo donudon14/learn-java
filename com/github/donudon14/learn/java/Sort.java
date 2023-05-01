de75474ebf54e03cf51ce79d2904037788c478d4
Anatoliy Ruslanov <k1ck001@mail.ru>
2023-05-01 15:00:25 +0100

    Sort

---
+++ com/github/donudon14/learn/java/Sort.java
@@ -0,0 +1,43 @@
+package com.github.donudon14.learn.java;
+
+import java.util.Comparator;
+import static com.github.donudon14.learn.java.Arrays.greaterThan;
+import static java.util.Objects.checkFromToIndex;
+
+public final class Sort {
+    public static <T> boolean isSorted(
+        final T[] array,
+        final Comparator<? super T> comparator
+    ) {
+        return isSorted(array, 0, array.length, comparator);
+    }
+
+    public static <T> boolean isSorted(
+        final T[] array,
+        final int fromIndex,
+        final int toIndex,
+        final Comparator<? super T> comparator
+    ) {
+        return isSortedUntil(array, fromIndex, toIndex, comparator) == toIndex;
+    }
+
+    public static <T> int isSortedUntil(
+        final T[] array,
+        final Comparator<? super T> comparator
+    ) {
+        return isSortedUntil(array, 0, array.length, comparator);
+    }
+
+    public static <T> int isSortedUntil(
+        final T[] array,
+        int fromIndex,
+        final int toIndex,
+        final Comparator<? super T> comparator
+    ) {
+        checkFromToIndex(fromIndex, toIndex, array.length);
+        for (; fromIndex + 1 < toIndex; ++fromIndex)
+            if (greaterThan(array, fromIndex, fromIndex + 1, comparator))
+                return fromIndex + 1;
+        return toIndex;
+    }
+}
